9f808e9da4646d17bb45a208b222ee4b27f3877e
Michael Belmear <90008055+mbelmear@users.noreply.github.com>
2023-04-01 15:55:35 -0600

    Delete src directory

--- src/DiscountBehavior.java
+++
@@ -1,3 +0,0 @@
-public interface DiscountBehavior {
-    public float getDiscountPercent();
-}   
--- src/MilitaryDiscount.java
+++
@@ -1,5 +0,0 @@
-public class MilitaryDiscount implements DiscountBehavior{
-    public float getDiscountPercent(){
-        return 0.10f;
-    }
-}
